75bcd47c517a3e4f4cea93b682ef86676f9e1c04
yoloex <jason.lanwin@gmail.com>
2023-09-13 00:47:12 +0900

    example for slicing audio

--- slice.py
+++ slice.py
@@ -15,5 +15,5 @@ wave_16 = librosa.resample(wave, orig_sr=sr, target_sr=16000)
 
 segments = librosa.effects.split(wave_16, top_db=13, frame_length=12800, hop_length=1200, ref=np.max)
 
-for i, segment in enumerate(segments):
-    sf.write(f'{i}.wav', wave_16[segment[0]:segment[1]], 16000)
+# for i, segment in enumerate(segments):
+#     sf.write(f'{i}.wav', wave_16[segment[0]:segment[1]], 16000)
