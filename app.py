e118b7c7d0481d6c457327a51b4ad83abc4102af
yoloex <jason.lanwin@gmail.com>
2023-09-13 00:47:18 +0900

    server added

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-import flask
+import io
+import numpy as np
+import soundfile as sf
+import librosa
+import warnings
 from flask import Flask, request, jsonify
+from inference import generate_transcription
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    pass
+    return jsonify({
+        'status': 'loaded'
+    })
 
 @app.route('/generate', methods=['POST'])
 def generate():
     if request.method == 'POST':
-        data = request.form['inputs']
-        print('inputs', data)
+        data = request.get_data()
+        wave, sr = sf.read(io.BytesIO(data), dtype='float32')
+
+        wave_16 = librosa.resample(wave, orig_sr=sr, target_sr=16000)
+        segments = librosa.effects.split(wave_16, top_db=13, frame_length=12800, hop_length=1200, ref=np.max)
+        generate_transcription([wave_16[segment[0]:segment[1]] for segment in segments])
+
+    return jsonify({'status': 'accpeted'})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
