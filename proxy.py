c3135bab9e5b00ed778fc1098e537d6a7ec92b40
yoloex <jason.lanwin@gmail.com>
2023-09-28 23:21:24 +0900

    remove cors

--- proxy.py
+++ proxy.py
@@ -1,11 +1,11 @@
 import random
 import requests
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 @app.route('/index')
