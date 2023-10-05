edd57baaa41f47a1e6c308a224bbae6c244e8869
yoloex <jason.lanwin@gmail.com>
2023-10-05 22:59:57 +0900

    update calc

--- calc.py
+++ calc.py
@@ -1,12 +1,15 @@
 with open('log/server.log') as f:
     logs = f.readlines()[-32:]
 
+total = 0
 sum = 0
 
-for i in range(0, 32, 2):
+for i in range(40):
     start = logs[i].find('finished in ')
 
-    # print(logs[i][start + len('finished in '):-2])
-    sum += float(logs[i][start + len('finished in '):-2])
+    if start > 0:
+        total += 1
+        # print(logs[i][start + len('finished in '):-2])
+        sum += float(logs[i][start + len('finished in '):-2])
 
-print(sum / 16)
+print(sum / total)
